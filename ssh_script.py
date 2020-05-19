f73137666cbe6f5e5aedf197109f2cdb77f364af
huybg-1975 <bui.gia.huy@sun-asterisk.com>
2020-05-19 21:54:24 +0700

    update code

--- ssh_script.py
+++ ssh_script.py
@@ -12,10 +12,6 @@ def main():
         for idx, raw_subnet in enumerate(subnets):
             # correct the current subnet string by remove '\n'
             subnet = raw_subnet[:-1]
-            
-            """ scan all ip in the current subnet
-            and just get IPs whose values have 'Pi' inside.
-            """
 
             # convert /24 /16 /8
             subnet_list = subnet.split('.')
@@ -28,23 +24,26 @@ def main():
                 subnet_list[1] = subnet_list[2] = subnet_list[3] = '0'
             subnet = '.'.join(subnet_list[:-1]) + '/{}'.format(subnet_list[-1])
 
+            # Currently, we will bypass for faster scanning
+            if subnet_list[-1] != '24':
+                continue
+
             os.system("sudo netdiscover -r {} -P | grep Pi | awk '{}' > ip_tmp{}.txt".format(subnet, '{print $1}', idx+1))
             with open('ip_tmp{}.txt'.format(idx+1), 'r') as IPs_list:
                 for ip in IPs_list:
                     ssh_client = paramiko.client.SSHClient()
                     ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
                     # Loop over each of IP address in the IP addresses above
-                    for raw_ip in ALL_IPs:
-                        print(f"current IP: {raw_ip}")
-                        try:
-                            ssh_client.connect(hostname=raw_ip, username='pi', password='Badien123')
-                            print(f"Your Raspberry Pi IP address is: {raw_ip}")
-                            print("----\nDONE.")
-                            return
-                        except paramiko.ssh_exception.NoValidConnectionsError:
-                            continue
-                        except paramiko.ssh_exception.AuthenticationException:
-                            continue
+                    try:
+                        ssh_client.connect(hostname=raw_ip, username='pi', password='Badien123')
+                        print(f"Your Raspberry Pi IP address is: {raw_ip}")
+                        print("----\nDONE.")
+                        return
+                    except paramiko.ssh_exception.NoValidConnectionsError:
+                        continue
+                    except paramiko.ssh_exception.AuthenticationException:
+                        continue
 
 
 if __name__ == '__main__':
