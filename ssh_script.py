79af7a843b9a1c94ee3fdbb8d5a6535cf2ad1dfd
hosjiu <bui.gia.huy@sun-asterisk.com>
2020-05-19 17:28:23 +0700

    update for working remote

--- ssh_script.py
+++ ssh_script.py
@@ -4,8 +4,18 @@ import paramiko
 if __name__ == '__main__':
     print('Scanning all available IP addresses in your network ...')
     import os
-    os.system('arp -a > net_info.txt')
+    os.system(r"ip a | grep -oE '\b([0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]{1,2}\b' | grep -v '^127' > net_info.txt")
     
+    # Scan IPs over all possible subnetwork
+    with open('subnets.txt', 'r') as subnets:
+        for raw_subnet in subnets:
+            # correct the current subnet string by remove '\n'
+            subnet = raw_subnet[:-1]
+            
+            # scan all ip in the current subnet
+            os.system('sudo netdiscover -r {subnet} -P > ')
+            
+
     """ Read net_info.txt file 
     and get list of valid IP addresses.
     """
@@ -27,8 +37,9 @@ if __name__ == '__main__':
     ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     # Loop over each of IP address in the IP addresses above
     for raw_ip in ALL_IPs:
+        print(f"current IP: {raw_ip}")
         try:
-            o = ssh_client.connect(hostname=raw_ip, username='pi', password='Badien123')
+            ssh_client.connect(hostname=raw_ip, username='pi', password='Badien123', timeout=5.)
             print(f"Your Raspberry Pi IP address is: {raw_ip}")
             break
         except paramiko.ssh_exception.NoValidConnectionsError:
